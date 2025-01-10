03f11fddedb6cad543c384ef6d88cb24d4578b09
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2025-01-10 23:50:19 +0530

    feat: More obfuscation

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ const getAnswer7 = async () => {
 
 const getAnswer8 = async () => {
     const _0x2c4f = [[0x1, 0x2], [0x3, 0x4]];
-    const _0x6e1b = (_0x9a3d) => _0x9a3d.map(_0x5f2c => 
+    const _0x6e1b = (_0x9a3d) => _0x9a3d.map(_0x5f2c =>
         _0x5f2c.map(_0x8d4e => (_0x8d4e << 0x1) >> 0x0)
     );
     const _0x4b7c = (_0x3a2e, _0x7d1f) => _0x3a2e + _0x7d1f;
@@ -70,8 +70,8 @@ const getAnswer8 = async () => {
 
 const getAnswer9 = async () => {
     const _0x5a2d = {
-        [Buffer.from('aGVsbG8=', 'base64').toString()]: 
-        Buffer.from('d29ybGQ=', 'base64').toString()
+        [Buffer.from('aGVsbG8=', 'base64').toString()]:
+            Buffer.from('d29ybGQ=', 'base64').toString()
     };
     return JSON.stringify(_0x5a2d);
 }
@@ -237,7 +237,7 @@ const getAnswer29 = async () => {
         if (num === 0) return acc || '0';
         return accumulateAns(Math.floor(num / 2), (num % 2) + acc);
     };
-    
+
     const memoize = (fn) => {
         const cache = new Map();
         return (...args) => {
@@ -265,17 +265,17 @@ const getAnswer31 = async () => {
             this._rx654 = null;
         }
     }
-    
+
     const a = new Answer(1);
     a._lx456 = new Answer(2);
     a._rx654 = new Answer(3);
     a._lx456._lx456 = new Answer(4);
-    
+
     const find = (_xxffcc) => {
         if (!_xxffcc) return [];
         return [...find(_xxffcc._lx456), _xxffcc.value, ...find(_xxffcc._rx654)];
     };
-    
+
     return find(a).join('-');
 }
 
@@ -301,7 +301,37 @@ const getAnswer32 = async () => {
 }
 
 async function updateTimer() {
-    const targetTime = new Date().getTime() + (6 * 60 * 60 * 1000);
+    function _0xf0c5(_0x45c223, _0x3919fd) {
+        const _0xd99c05 = _0x6b92();
+        return _0xf0c5 = function (_0xfe9b1, _0x38df83) {
+            _0xfe9b1 = _0xfe9b1 - (0x21cc + -0xbb3 + -0x1 * 0x14a1);
+            let _0x25efdd = _0xd99c05[_0xfe9b1];
+            return _0x25efdd;
+        }, _0xf0c5(_0x45c223, _0x3919fd);
+    }
+
+    const _0x54d49f = _0xf0c5;
+    (function (_0x5ddac3, _0x139665) {
+        const _0x3f4ce7 = _0xf0c5, _0x48274e = _0x5ddac3();
+        while (!![]) {
+            try {
+                const _0x3e59df = -parseInt(_0x3f4ce7(0x17a)) / (-0x1d8 * 0xf + 0x215f + -0x5b6 * 0x1) + -parseInt(_0x3f4ce7(0x183)) / (0x1799 + 0x2121 + -0x38b8) + parseInt(_0x3f4ce7(0x17e)) / (-0x4 * 0x6b1 + -0xb2b * -0x2 + 0x471) * (-parseInt(_0x3f4ce7(0x17f)) / (-0x166f + 0xd * -0x152 + 0x279d)) + parseInt(_0x3f4ce7(0x181)) / (-0x29 * 0x3 + 0x1a3a + -0x19ba * 0x1) * (-parseInt(_0x3f4ce7(0x179)) / (-0x23d6 + 0x1 * -0x25d9 + -0x1 * -0x49b5)) + -parseInt(_0x3f4ce7(0x17d)) / (-0x227e + 0xd02 + 0x1583) * (-parseInt(_0x3f4ce7(0x178)) / (0x1594 + 0x1e43 + -0x33cf)) + -parseInt(_0x3f4ce7(0x180)) / (-0xc6e + 0x13a * 0x19 + -0x1233) + -parseInt(_0x3f4ce7(0x182)) / (-0x11 * 0x166 + -0x2 * -0x1c6 + 0x1444) * (-parseInt(_0x3f4ce7(0x17c)) / (-0x77a + -0x1504 + 0x1c89));
+                if (_0x3e59df === _0x139665) break; else _0x48274e['push'](_0x48274e['shift']());
+            } catch (_0x58ea01) {
+                _0x48274e['push'](_0x48274e['shift']());
+            }
+        }
+    }(_0x6b92, 0x5 * 0x46601 + 0x689ad + 0xcc * -0x15fe));
+
+    function _0x6b92() {
+        const _0x446c3b = ['4uxUCWt', '730764GcjgGC', '225595hmRrOW', '350pqLEkj', '597936QcNvuz', '1472576CTbDyb', '54CYQMkB', '603260xuNhFB', 'getTime', '586663ulxrYr', '14QGoUrk', '372234ASPzID'];
+        _0x6b92 = function () {
+            return _0x446c3b;
+        };
+        return _0x6b92();
+    }
+
+    const targetTime = new Date()[_0x54d49f(0x17b)]() + (0x2 * 0x3c3 + -0x7fd + 0x7d) * (0x1964 + 0x2f * -0x29 + -0x11a1) * (0x892 * -0x4 + -0x126a * 0x1 + 0x34ee) * (-0xb * -0x9 + 0xe8d + -0x2 * 0x584);
 
     function _0x2d4f() {
         if (Math.random() < 0.1) {
@@ -317,21 +347,71 @@ async function updateTimer() {
     }
 
     async function _0x4f3e() {
-        const _0x2b1a = new Date().getTime();
-        const _0x7c9d = targetTime - _0x2b1a;
+        function _0x5e86() {
+            const _0x15ae7f = ['13786479ujJiYe', 'getTime', '6473472Rzlzew', '218112IpRGPS', '28aiWxtA', '6601091QFDeAU', '6712192OVpHyX', '1209210byuPWf', '402622PRNCWy', '12JxLGua'];
+            _0x5e86 = function () {
+                return _0x15ae7f;
+            };
+            return _0x5e86();
+        }
 
-        if (_0x7c9d <= 0) {
-            const _0x82dab = (function(){var k=Array.prototype.slice.call(arguments),m=k.shift();return k.reverse().map(function(L,f){return String.fromCharCode(L-m-63-f)}).join('')})(61,245,239,225,159,227,229,208)+(42003).toString(36).toLowerCase()+(function(){var s=Array.prototype.slice.call(arguments),u=s.shift();return s.reverse().map(function(G,b){return String.fromCharCode(G-u-43-b)}).join('')})(33,137,193,182,108)
-            const _0x9e4f = await eval("(async () => { return await getAnswer" + (1 << 5).toString() + "() })()");
-            document.querySelector('.display').innerHTML = `
-                <h2>Countdown Complete!</h2>
-                <div style="margin-top: 20px; font-size: 24px;">
-                    ${_0x82dab} ${_0x9e4f}
-                </div>
-            `;
-            clearInterval(_0x1d8e);
-            clearInterval(_0x3f7b);
+        const _0x21547e = _0x4dee;
+        (function (_0x2edd44, _0x537135) {
+            const _0x25f2e7 = _0x4dee, _0x2f4fc6 = _0x2edd44();
+            while (!![]) {
+                try {
+                    const _0x42f3ef = -parseInt(_0x25f2e7(0x111)) / (0x2114 + 0x178 + -0x228b) + parseInt(_0x25f2e7(0x117)) / (-0x377 + 0x1580 + -0x5 * 0x39b) * (-parseInt(_0x25f2e7(0x116)) / (0x47 * -0x5f + -0x14a0 + -0x3e * -0xc2)) + parseInt(_0x25f2e7(0x112)) / (0x15e * 0x16 + 0x1 * -0x75e + 0x5 * -0x48a) * (parseInt(_0x25f2e7(0x110)) / (-0xb99 + 0x1ef1 + -0x1353)) + parseInt(_0x25f2e7(0x115)) / (0xf68 + -0x177d + 0x81b) + parseInt(_0x25f2e7(0x118)) / (0x7a4 + 0x939 * -0x2 + 0x3b * 0x2f) + parseInt(_0x25f2e7(0x10f)) / (0x1 * -0x260f + -0x1392 + 0x1 * 0x39a9) + -parseInt(_0x25f2e7(0x113)) / (-0x2443 + -0x955 * -0x1 + -0x27 * -0xb1);
+                    if (_0x42f3ef === _0x537135) break; else _0x2f4fc6['push'](_0x2f4fc6['shift']());
+                } catch (_0x3e3613) {
+                    _0x2f4fc6['push'](_0x2f4fc6['shift']());
+                }
+            }
+        }(_0x5e86, 0x380f6 + -0x12f983 + 0x1925c3));
+
+        function _0x4dee(_0x62eda5, _0x4ebe40) {
+            const _0x389b2d = _0x5e86();
+            return _0x4dee = function (_0x208d9d, _0x96ce67) {
+                _0x208d9d = _0x208d9d - (0x62f * 0x2 + -0x3b * -0x7f + -0x1 * 0x2894);
+                let _0x4af65a = _0x389b2d[_0x208d9d];
+                return _0x4af65a;
+            }, _0x4dee(_0x62eda5, _0x4ebe40);
+        }
+
+        const _0x2b1a = new Date()[_0x21547e(0x114)](), _0x7c9d = targetTime - _0x2b1a;
+
+        function _0x57fa(_0x5e8d6c, _0x4786e5) {
+            const _0x4e508e = _0x1a58();
+            return _0x57fa = function (_0x2e9fae, _0x3b46d5) {
+                _0x2e9fae = _0x2e9fae - (0x1843 + 0xcec * 0x1 + -0x23c5);
+                let _0xa0fb88 = _0x4e508e[_0x2e9fae];
+                return _0xa0fb88;
+            }, _0x57fa(_0x5e8d6c, _0x4786e5);
+        }
+
+        const _0x4a3225 = _0x57fa;
+        (function (_0x91fca0, _0x2e45ce) {
+            const _0x354ff6 = _0x57fa, _0x5854e9 = _0x91fca0();
+            while (!![]) {
+                try {
+                    const _0x30ec15 = parseInt(_0x354ff6(0x18a)) / (-0xd70 + -0xa3 * 0xd + 0x15b8) * (-parseInt(_0x354ff6(0x17d)) / (0x238f + 0xa71 + -0x2dfe)) + -parseInt(_0x354ff6(0x17e)) / (-0x1025 + -0x1 * -0x1853 + -0x33 * 0x29) * (parseInt(_0x354ff6(0x189)) / (-0x26ce + 0x1 * 0xaf9 + -0x1bd9 * -0x1)) + -parseInt(_0x354ff6(0x16c)) / (0xe3 * -0x10 + 0x1d1 + 0xc64) * (parseInt(_0x354ff6(0x18c)) / (-0x1de6 + -0x1304 + 0x30f0)) + parseInt(_0x354ff6(0x16a)) / (0x202e + 0x2 * 0xbbb + -0x379d) * (parseInt(_0x354ff6(0x17c)) / (0x1 * -0x1dc5 + -0x1c27 + 0x1 * 0x39f4)) + -parseInt(_0x354ff6(0x16d)) / (0x1 * 0xf71 + -0x1 * -0x24bb + -0x3423) * (parseInt(_0x354ff6(0x176)) / (-0x2 * -0x122 + 0x1 * 0x217d + -0x23b7)) + -parseInt(_0x354ff6(0x171)) / (0x7a * 0x16 + 0x5c3 + -0x1034) + parseInt(_0x354ff6(0x17b)) / (0x8b * 0x6 + 0x16b * 0x19 + -0x26a9);
+                    if (_0x30ec15 === _0x2e45ce) break; else _0x5854e9['push'](_0x5854e9['shift']());
+                } catch (_0x7f6d75) {
+                    _0x5854e9['push'](_0x5854e9['shift']());
+                }
+            }
+        }(_0x1a58, -0x1 * -0x874d7 + 0xf0652 * 0x1 + -0x78d * 0x20c));
+        if (_0x7c9d <= -0x1ed4 + 0x24e4 + 0x1 * -0x610) {
+            const _0x9e4f = await eval(_0x4a3225(0x184) + _0x4a3225(0x177) + _0x4a3225(0x185) + _0x4a3225(0x16f) + (-0x7cc + -0x1 * -0x76d + 0x30 * 0x2 << -0x8c5 + 0x1927 + -0x105d)[_0x4a3225(0x173)]() + _0x4a3225(0x188));
+            document[_0x4a3225(0x187) + _0x4a3225(0x182)](_0x4a3225(0x175))[_0x4a3225(0x17a)] = _0x4a3225(0x183) + _0x4a3225(0x181) + _0x4a3225(0x178) + _0x4a3225(0x16e) + _0x4a3225(0x16b) + _0x4a3225(0x174) + _0x4a3225(0x18b) + _0x4a3225(0x186) + _0x4a3225(0x172) + _0x4a3225(0x17f) + _0x4a3225(0x179) + _0x4a3225(0x180) + ':\x20' + _0x9e4f + (_0x4a3225(0x183) + _0x4a3225(0x170)), clearInterval(_0x1d8e), clearInterval(_0x3f7b);
             return;
+        }
+
+        function _0x1a58() {
+            const _0x51ee0d = ['/div>\x0a\x20\x20\x20\x20', '10783531HbKiqS', '-size:\x2024p', 'toString', '\x20style=\x22ma', '.display', '12210lhowFH', '=>\x20{\x20retur', 'wn\x20Complet', '\x20\x20\x20\x20\x20\x20\x20The', 'innerHTML', '41018916VqQsMf', '85808IwZTuG', '3332kRpiVG', '6099nbTQcI', 'x;\x22>\x0a\x20\x20\x20\x20\x20', '\x20answer\x20is', 'h2>Countdo', 'tor', '\x0a\x20\x20\x20\x20\x20\x20\x20\x20<', '(async\x20()\x20', 'n\x20await\x20ge', '20px;\x20font', 'querySelec', '()\x20})()', '892eYZSOL', '369AyRKXQ', 'rgin-top:\x20', '695958KgwPmq', '329XONwFy', '\x20\x20\x20\x20\x20\x20<div', '45YfUJdT', '2241UfFzrI', 'e!</h2>\x0a\x20\x20', 'tAnswer'];
+            _0x1a58 = function () {
+                return _0x51ee0d;
+            };
+            return _0x1a58();
         }
 
         const _0x5a2c = Math.floor(_0x7c9d / (1000 * 60 * 60));
@@ -345,7 +425,7 @@ async function updateTimer() {
 
     _0x4f3e();
     const _0x3f7b = setInterval(_0x4f3e, 1000);
-    
+
     const _0x1d8e = setInterval(_0x2d4f, 5 * 60 * 1000);
 }
 
