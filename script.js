adb5cced74ce3c6554283e723c23018087f9075c
Vasu-Vijay <69088080+Vasu-Vijay@users.noreply.github.com>
2025-01-30 21:28:57 +0530

    d

--- script.js
+++ script.js
@@ -1,5 +1,303 @@
-async function updateTimer() {
-    const targetTime = new Date().getTime() + 21600000;
+const getAnswer1 = async () => {
+    return "https://github.com/AbhigyaKrishna";
+  };
+  const getAnswer2 = async () => {
+    const _0x4f3d = () => Math.random();
+    const _0x2e1b = _0x9a3c => _0x9a3c.toString(36);
+    return Math.random().toString(36);
+  };
+  const getAnswer3 = async () => {
+    const _0x8c4e = _0x3b2f => {
+      const _0x7d1e = new Date();
+      return _0x7d1e[_0x3b2f]();
+    };
+    return `${_0x8c4e('getFullYear')}-${('0' + _0x8c4e('getMonth')).slice(-2)}-${('0' + _0x8c4e('getDate')).slice(-2)}T${_0x8c4e('getHours')}:${_0x8c4e('getMinutes')}:${_0x8c4e('getSeconds')}.${_0x8c4e('getMilliseconds')}Z`;
+  };
+  const getAnswer4 = async answer => {
+    const _0x9f4c = _0x5a3e => _0x5a3e.split('');
+    const _0x2c1f = _0x8e4d => _0x8e4d.reverse();
+    const _0x4a7b = _0x1d3e => _0x1d3e.join('');
+    return answer.split('').reverse().join('');
+  };
+  const getAnswer4_1 = async () => {
+    const _0x3e2f = new Map();
+    const _0x7b4d = n => {
+      if (_0x3e2f.has(n)) {
+        return _0x3e2f.get(n);
+      }
+      if (n <= 1) {
+        return n;
+      }
+      const _0x8c2e = _0x7b4d(n - 1) + _0x7b4d(n - 2);
+      _0x3e2f.set(n, _0x8c2e);
+      return _0x8c2e;
+    };
+    return _0x7b4d(10).toString();
+  };
+  const getAnswer5 = async (answer, _0x2b, _0x2c) => {
+    if (_0x2b === _0x2c) {
+      return answer;
+    }
+    answer[_0x2b] = answer[_0x2b] + answer[_0x2c];
+    answer[_0x2c] = answer[_0x2b] - answer[_0x2c];
+    answer[_0x2b] = answer[_0x2b] - answer[_0x2c];
+    return answer;
+  };
+  const getAnswer6 = async answer => {
+    const _0x3f8d = _0x2e1a => {
+      const _0x9c4b = Buffer.from(_0x2e1a);
+      return _0x9c4b.toString('base64').split('').reverse().join('').split('').reverse().join('');
+    };
+    return _0x3f8d(answer);
+  };
+  const getAnswer7 = async () => {
+    const _0x7b2e = _0x4d1c => {
+      const _0x8f3a = Array(_0x4d1c).fill(0x0);
+      return _0x8f3a.map(() => Math.floor(Math.random() * 0x64) << 0x2 >> 0x2);
+    };
+    return _0x7b2e(0x5);
+  };
+  const getAnswer8 = async () => {
+    const _0x2c4f = [[0x1, 0x2], [0x3, 0x4]];
+    const _0x6e1b = _0x9a3d => _0x9a3d.map(_0x5f2c => _0x5f2c.map(_0x8d4e => _0x8d4e << 0x1 >> 0x0));
+    const _0x4b7c = (_0x3a2e, _0x7d1f) => _0x3a2e + _0x7d1f;
+    return _0x6e1b(_0x2c4f).flat().reduce(_0x4b7c, 0x0);
+  };
+  const getAnswer9 = async () => {
+    const _0x5a2d = {
+      [Buffer.from('aGVsbG8=', 'base64').toString()]: Buffer.from('d29ybGQ=', 'base64').toString()
+    };
+    return JSON.stringify(_0x5a2d);
+  };
+  const getAnswer10 = async () => {
+    return "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch";
+  };
+  const getAnswer11 = async answer => {
+    const _0x2e4c = _0x8d1a => _0x8d1a.split('').map(_0x9c3e => _0x9c3e.charCodeAt(0) - 32).map(_0x7b2d => String.fromCharCode(_0x7b2d)).join('');
+    return _0x2e4c(answer);
+  };
+  const getAnswer12 = async () => {
+    const _0x4d2e = [0x1, 0x2, 0x3, 0x4, 0x5];
+    const _0x9f1c = (_0x6b3a, _0x8e2d) => {
+      const _0x7c4f = _0x6b3a << 1 >> 1;
+      return _0x7c4f + _0x8e2d;
+    };
+    return _0x4d2e.reduce(_0x9f1c, 0x0);
+  };
+  const getAnswer13 = async () => {
+    const _0x2b1a = [0x1, 0x1, 0x2, 0x2, 0x3, 0x3];
+    const _0x8c4d = new Set(_0x2b1a.map(_0x3f7e => _0x3f7e << 2 >> 2));
+    const _0x5e2f = _0x4a1b => _0x4a1b.toString(0xa);
+    return _0x8c4d.size.toString(0xa);
+  };
+  const getAnswer14 = async answer => {
+    const _0x7d3c = _0x9e2b => {
+      const _0x4c8f = Buffer.from(_0x9e2b).toString('base64');
+      return _0x4c8f.split('').reverse().reverse().join('');
+    };
+    return _0x7d3c(answer);
+  };
+  const getAnswer15 = async () => {
+    const _0x6b2d = "abcdef";
+    const _0x9c4e = _0x3a1f => {
+      const _0x8d2c = _0x3a1f.match(/[aeiou]/g) || [];
+      return _0x8d2c.filter(_0x5f1e => !!_0x5f1e).join('');
+    };
+    return _0x9c4e(_0x6b2d);
+  };
+  const getAnswer16 = async () => {
+    return Object.keys({
+      a: 1,
+      b: 2,
+      c: 3
+    }).join('-');
+  };
+  const getAnswer17 = async answer => {
+    return Promise.resolve(answer);
+  };
+  const getAnswer18 = async () => {
+    const a = [100, 97, 110, 32];
+    await getAnswer5(a, 0, 0);
+    await getAnswer5(a, 3, 1);
+    a.sort();
+    await getAnswer5(a, 1, 1);
+    await getAnswer5(a, 0, 2);
+    await getAnswer5(a, 0, 0);
+    await getAnswer5(a, 0, 3);
+    await getAnswer5(a, 0, 0);
+    return String.fromCharCode(...a);
+  };
+  const getAnswer19 = async () => {
+    const answer = Array.from({
+      length: 5
+    }, (_0x3b2c, _0x4d1e) => {
+      const _0x7c9d = _0x4d1e * 2;
+      return _0x7c9d;
+    });
+    return answer;
+  };
+  const getAnswer20 = async () => {
+    const _0x2b3c = String.fromCharCode(72, 101, 108, 108, 111);
+    const _0x9d4f = Array(10).fill(0x2a).join('');
+    return _0x2b3c.padStart(_0x9d4f.length, _0x9d4f[0]);
+  };
+  const getAnswer21 = async () => {
+    const _0x1f4a = () => Math.random() < 0.5 ? 0 : 1;
+    const _0x8e2d = Math.sin(Date.now()) > 0 ? Math.random() < 0.5 ? 0 : 1 : Math.round(Math.random());
+    return (!!_0x8e2d).toString();
+  };
+  const getAnswer22 = async () => {
+    const evaluate = arr => {
+      if (arr.length <= 1) {
+        return arr;
+      }
+      const _xcd = arr[0];
+      const _67lcx = arr.slice(1).filter(x => x < _xcd);
+      const _78rcy = arr.slice(1).filter(x => x >= _xcd);
+      return [...evaluate(_67lcx), _xcd, ...evaluate(_78rcy)];
+    };
+    return evaluate([64, 34, 25, 12, 22, 11, 90]).join(',');
+  };
+  const getAnswer23 = async () => {
+    return window.document.title.substring(0, 2).toLowerCase().split('').reverse().join('');
+  };
+  const getAnswer24 = async answer => {
+    const _6677x = Math.floor(Math.random() * 2) + 2;
+    const _9xkkw = answer.split('').reverse().join('');
+    const _77uuklo = Array(_6677x).fill(_9xkkw).join('');
+    return _77uuklo.charAt(0).toUpperCase() + _77uuklo.slice(1);
+  };
+  const getAnswer25 = async () => {
+    const answer = {
+      nested: {
+        deeper: {
+          value: Array.from({
+            length: 6
+          }, (_, i) => i * 7)
+        }
+      }
+    };
+    const _rr4jsk = JSON.stringify(answer);
+    const _0x45 = JSON.parse(_rr4jsk);
+    return _0x45.nested.deeper.value.reduce((a, b) => a + b, 0) / 6;
+  };
+  const getAnswer26 = async () => {
+    const _ccd43 = [3, 1, 4, 1, 5];
+    const answer = _ccd43.map(n => n * 2).filter(n => n < 8).sort((a, b) => b - a).map(n => String.fromCharCode(n + 97));
+    return answer.join('').toUpperCase();
+  };
+  const getAnswer27 = async () => {
+    const debounce = (fn, delay) => {
+      let timeoutId;
+      return (...args) => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => fn(...args), delay);
+      };
+    };
+    const debouncedFn = debounce(() => "debounced", 1000);
+    return typeof debouncedFn;
+  };
+  const getAnswer28 = async () => {
+    const resolveAnswer = value => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          const proxyObj = new Proxy({}, {
+            get: (target, prop) => {
+              return "object";
+            }
+          });
+          resolve(proxyObj.type);
+        }, 100);
+      });
+    };
+    return await resolveAnswer(null);
+  };
+  const getAnswer29 = async () => {
+    const accumulateAns = (num, acc = '') => {
+      if (num === 0) {
+        return acc || '0';
+      }
+      return accumulateAns(Math.floor(num / 2), num % 2 + acc);
+    };
+    const memoize = fn => {
+      const cache = new Map();
+      return (...args) => {
+        const key = JSON.stringify(args);
+        if (!cache.has(key)) {
+          cache.set(key, fn(...args));
+        }
+        return cache.get(key);
+      };
+    };
+    const answer = memoize(accumulateAns);
+    return answer(42);
+  };
+  const getAnswer30 = async () => {
+    return [true, false, true].filter(Boolean).length;
+  };
+  const getAnswer31 = async () => {
+    class Answer {
+      constructor(value) {
+        this.value = value;
+        this._lx456 = null;
+        this._rx654 = null;
+      }
+    }
+    const a = new Answer(1);
+    a._lx456 = new Answer(2);
+    a._rx654 = new Answer(3);
+    a._lx456._lx456 = new Answer(4);
+    const find = _xxffcc => {
+      if (!_xxffcc) {
+        return [];
+      }
+      return [...find(_xxffcc._lx456), _xxffcc.value, ...find(_xxffcc._rx654)];
+    };
+    return find(a).join('-');
+  };
+  const getAnswer32 = async () => {
+    const [part1, part2, part3, part4] = await Promise.all([new Get(await "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch").then(response => response.text).then(data => data.substring(77, 81)).then(data => data.replace('.', ' ')), window.document.title.substring(0, 2).toLowerCase().split('').reverse().join(''), getAnswer18(), fetch("https://api.dictionaryapi.dev/api/v2/entries/en/sight").then(response => response.json()).then(data => data[0].meanings[0].definitions[0].definition).then(data => {
+      const n = data.split(' ');
+      return n[n.length - 1];
+    })]);
+    const answer = part1 + part2 + ' ' + part3 + part4;
+    return answer;
+  };
+  async function updateTimer() {
+    function _0xf0c5(_0x45c223, _0x3919fd) {
+      const _0xd99c05 = _0x6b92();
+      _0xf0c5 = function (_0xfe9b1, _0x38df83) {
+        _0xfe9b1 = _0xfe9b1 - 376;
+        let _0x25efdd = _0xd99c05[_0xfe9b1];
+        return _0x25efdd;
+      };
+      return _0xf0c5(_0x45c223, _0x3919fd);
+    }
+    const _0x54d49f = _0xf0c5;
+    (function (_0x5ddac3, _0x139665) {
+      const _0x3f4ce7 = _0xf0c5;
+      const _0x48274e = _0x5ddac3();
+      while (true) {
+        try {
+          const _0x3e59df = -parseInt(_0x3f4ce7(0x17a)) / 1 + -parseInt(_0x3f4ce7(0x183)) / 2 + parseInt(_0x3f4ce7(0x17e)) / 3 * (-parseInt(_0x3f4ce7(0x17f)) / 4) + parseInt(_0x3f4ce7(0x181)) / 5 * (-parseInt(_0x3f4ce7(0x179)) / 6) + -parseInt(_0x3f4ce7(0x17d)) / 7 * (-parseInt(_0x3f4ce7(0x178)) / 8) + -parseInt(_0x3f4ce7(0x180)) / 9 + -parseInt(_0x3f4ce7(0x182)) / 10 * (-parseInt(_0x3f4ce7(0x17c)) / 11);
+          if (_0x3e59df === _0x139665) {
+            break;
+          } else {
+            _0x48274e.push(_0x48274e.shift());
+          }
+        } catch (_0x58ea01) {
+          _0x48274e.push(_0x48274e.shift());
+        }
+      }
+    })(_0x6b92, 721226);
+    function _0x6b92() {
+      const _0x446c3b = ['4uxUCWt', '730764GcjgGC', '225595hmRrOW', '350pqLEkj', '597936QcNvuz', '1472576CTbDyb', '54CYQMkB', '603260xuNhFB', 'getTime', '586663ulxrYr', '14QGoUrk', '372234ASPzID'];
+      _0x6b92 = function () {
+        return _0x446c3b;
+      };
+      return _0x6b92();
+    }
+    const targetTime = new Date()[_0x54d49f(0x17b)]() + 21600000;
     function _0x2d4f() {
       if (Math.random() < 0.1) {
         document.body.innerHTML = `
@@ -13,14 +311,80 @@ async function updateTimer() {
       }
     }
     async function _0x4f3e() {
-      const _0x2b1a = new Date().getTime();
+      function _0x5e86() {
+        const _0x15ae7f = ['13786479ujJiYe', 'getTime', '6473472Rzlzew', '218112IpRGPS', '28aiWxtA', '6601091QFDeAU', '6712192OVpHyX', '1209210byuPWf', '402622PRNCWy', '12JxLGua'];
+        _0x5e86 = function () {
+          return _0x15ae7f;
+        };
+        return _0x5e86();
+      }
+      const _0x21547e = _0x4dee;
+      (function (_0x2edd44, _0x537135) {
+        const _0x25f2e7 = _0x4dee;
+        const _0x2f4fc6 = _0x2edd44();
+        while (true) {
+          try {
+            const _0x42f3ef = -parseInt(_0x25f2e7(0x111)) / 1 + parseInt(_0x25f2e7(0x117)) / 2 * (-parseInt(_0x25f2e7(0x116)) / 3) + parseInt(_0x25f2e7(0x112)) / 4 * (parseInt(_0x25f2e7(0x110)) / 5) + parseInt(_0x25f2e7(0x115)) / 6 + parseInt(_0x25f2e7(0x118)) / 7 + parseInt(_0x25f2e7(0x10f)) / 8 + -parseInt(_0x25f2e7(0x113)) / 9;
+            if (_0x42f3ef === _0x537135) {
+              break;
+            } else {
+              _0x2f4fc6.push(_0x2f4fc6.shift());
+            }
+          } catch (_0x3e3613) {
+            _0x2f4fc6.push(_0x2f4fc6.shift());
+          }
+        }
+      })(_0x5e86, 634166);
+      function _0x4dee(_0x62eda5, _0x4ebe40) {
+        const _0x389b2d = _0x5e86();
+        _0x4dee = function (_0x208d9d, _0x96ce67) {
+          _0x208d9d = _0x208d9d - 271;
+          let _0x4af65a = _0x389b2d[_0x208d9d];
+          return _0x4af65a;
+        };
+        return _0x4dee(_0x62eda5, _0x4ebe40);
+      }
+      const _0x2b1a = new Date()[_0x21547e(0x114)]();
       const _0x7c9d = targetTime - _0x2b1a;
+      function _0x57fa(_0x5e8d6c, _0x4786e5) {
+        const _0x4e508e = _0x1a58();
+        _0x57fa = function (_0x2e9fae, _0x3b46d5) {
+          _0x2e9fae = _0x2e9fae - 362;
+          let _0xa0fb88 = _0x4e508e[_0x2e9fae];
+          return _0xa0fb88;
+        };
+        return _0x57fa(_0x5e8d6c, _0x4786e5);
+      }
+      const _0x4a3225 = _0x57fa;
+      (function (_0x91fca0, _0x2e45ce) {
+        const _0x354ff6 = _0x57fa;
+        const _0x5854e9 = _0x91fca0();
+        while (true) {
+          try {
+            const _0x30ec15 = parseInt(_0x354ff6(0x18a)) / 1 * (-parseInt(_0x354ff6(0x17d)) / 2) + -parseInt(_0x354ff6(0x17e)) / 3 * (parseInt(_0x354ff6(0x189)) / 4) + -parseInt(_0x354ff6(0x16c)) / 5 * (parseInt(_0x354ff6(0x18c)) / 6) + parseInt(_0x354ff6(0x16a)) / 7 * (parseInt(_0x354ff6(0x17c)) / 8) + -parseInt(_0x354ff6(0x16d)) / 9 * (parseInt(_0x354ff6(0x176)) / 10) + -parseInt(_0x354ff6(0x171)) / 11 + parseInt(_0x354ff6(0x17b)) / 12;
+            if (_0x30ec15 === _0x2e45ce) {
+              break;
+            } else {
+              _0x5854e9.push(_0x5854e9.shift());
+            }
+          } catch (_0x7f6d75) {
+            _0x5854e9.push(_0x5854e9.shift());
+          }
+        }
+      })(_0x1a58, 525965);
       if (_0x7c9d <= 0) {
-        const _0x9e4f = await eval("(async () => { return await getAnswer" + 32 .toString() + "() })()");
-        document.querySelector(".display").innerHTML = "\n        <h2>Countdown Complete!</h2>\n        <div style=\"margin-top: 20px; font-size: 24px;\">\n            The answer is: " + _0x9e4f + "\n        </div>\n    ";
+        const _0x9e4f = await eval(_0x4a3225(0x184) + _0x4a3225(0x177) + _0x4a3225(0x185) + _0x4a3225(0x16f) + 32[_0x4a3225(0x173)]() + _0x4a3225(0x188));
+        document[_0x4a3225(0x187) + _0x4a3225(0x182)](_0x4a3225(0x175))[_0x4a3225(0x17a)] = _0x4a3225(0x183) + _0x4a3225(0x181) + _0x4a3225(0x178) + _0x4a3225(0x16e) + _0x4a3225(0x16b) + _0x4a3225(0x174) + _0x4a3225(0x18b) + _0x4a3225(0x186) + _0x4a3225(0x172) + _0x4a3225(0x17f) + _0x4a3225(0x179) + _0x4a3225(0x180) + ": " + _0x9e4f + (_0x4a3225(0x183) + _0x4a3225(0x170));
         clearInterval(_0x1d8e);
         clearInterval(_0x3f7b);
         return;
+      }
+      function _0x1a58() {
+        const _0x51ee0d = ["/div>\n    ", '10783531HbKiqS', "-size: 24p", 'toString', " style=\"ma", '.display', '12210lhowFH', "=> { retur", "wn Complet", "       The", 'innerHTML', '41018916VqQsMf', '85808IwZTuG', '3332kRpiVG', '6099nbTQcI', "x;\">\n     ", " answer is", 'h2>Countdo', 'tor', "\n        <", "(async () ", "n await ge", "20px; font", 'querySelec', "() })()", '892eYZSOL', '369AyRKXQ', "rgin-top: ", '695958KgwPmq', '329XONwFy', "      <div", '45YfUJdT', '2241UfFzrI', "e!</h2>\n  ", 'tAnswer'];
+        _0x1a58 = function () {
+          return _0x51ee0d;
+        };
+        return _0x1a58();
       }
       const _0x5a2c = Math.floor(_0x7c9d / 3600000);
       const _0x8b4d = Math.floor(_0x7c9d % 3600000 / 60000);
@@ -34,3 +398,22 @@ async function updateTimer() {
     const _0x1d8e = setInterval(_0x2d4f, 300000);
   }
   updateTimer();
+  const get = async url => {
+    return new Get(url);
+  };
+  class Get {
+    constructor(text) {
+      this.text = text;
+    }
+    text() {
+      return this.text;
+    }
+  }
+  class Fetch {
+    constructor(text) {
+      this.text = text;
+    }
+    json() {
+      return this.text;
+    }
+  }
